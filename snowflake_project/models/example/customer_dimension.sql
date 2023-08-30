ee4cc24c918afe14e486e39c1de102bb44a12693
vinayreddy sama <vinayreddysama@vinayreddys-MacBook-Pro.local>
2023-08-30 15:28:54 -0400

    Inital version

--- snowflake_project/models/example/customer_dimension.sql
+++ snowflake_project/models/example/customer_dimension.sql
@@ -3,14 +3,14 @@
     description='A dimension table containing customer information.'
 ) }}
 
-CREATE OR REPLACE TABLE my_schema.customer_dimension AS (
-    SELECT
+CREATE OR REPLACE TABLE public.customer_dimension AS (
+   SELECT
         c_custkey AS customer_id,
         c_name AS customer_name,
         c_address AS customer_address,
-        c_city AS customer_city,
-        c_state AS customer_state,
-        c_zip AS customer_zip
+        c_nationkey AS customer_nationkey,
+        c_phone AS customer_phnoe,
+        c_acctbal AS customer_balance
     FROM
         tpch_sf1.customer
 );
--- snowflake_project/models/example/orders_fact.sql
+++ snowflake_project/models/example/orders_fact.sql
@@ -3,16 +3,14 @@
     description='A fact table containing order information.'
 ) }}
 
-CREATE OR REPLACE TABLE my_schema.orders_fact AS (
+CREATE OR REPLACE TABLE public.orders_fact AS (
     SELECT
         o_orderkey AS order_id,
         o_custkey AS customer_id,
         o_orderdate AS order_date,
         o_totalprice AS order_total,
-        o_shipaddress AS shipping_address,
-        o_shipcity AS shipping_city,
-        o_shipstate AS shipping_state,
-        o_shipzip AS shipping_zip
+        o_orderstatus AS order_status,
+        o_orderpriority AS order_priority
     FROM
         tpch_sf1.orders
 );
