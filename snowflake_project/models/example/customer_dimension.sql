63417f1dc1e11f221e9f04f854c42d7bc24a43d0
vinayreddy sama <vinayreddysama@vinayreddys-MacBook-Pro.local>
2023-08-30 23:20:47 -0400

    Final version

--- snowflake_project/models/example/customer_dimension.sql
+++ snowflake_project/models/example/customer_dimension.sql
@@ -1,16 +1,14 @@
 {{ config(
     materialized='table',
-    description='A dimension table containing customer information.'
+    description='A fact table containing order information.'
 ) }}
 
-CREATE OR REPLACE TABLE public.customer_dimension AS (
-   SELECT
-        c_custkey AS customer_id,
-        c_name AS customer_name,
-        c_address AS customer_address,
-        c_nationkey AS customer_nationkey,
-        c_phone AS customer_phnoe,
-        c_acctbal AS customer_balance
-    FROM
-        tpch_sf1.customer
-);
+SELECT
+    c_custkey AS customer_id,
+    c_name AS customer_name,
+    c_address AS customer_address,
+    c_nationkey AS customer_nationkey,
+    c_phone AS customer_phnoe,
+    c_acctbal AS customer_balance
+FROM
+    snowflake_sample_data.tpch_sf1.customer
--- snowflake_project/models/example/orders_fact.sql
+++ snowflake_project/models/example/orders_fact.sql
@@ -3,14 +3,12 @@
     description='A fact table containing order information.'
 ) }}
 
-CREATE OR REPLACE TABLE public.orders_fact AS (
-    SELECT
-        o_orderkey AS order_id,
-        o_custkey AS customer_id,
-        o_orderdate AS order_date,
-        o_totalprice AS order_total,
-        o_orderstatus AS order_status,
-        o_orderpriority AS order_priority
-    FROM
-        tpch_sf1.orders
-);
+SELECT
+    o_orderkey AS order_id,
+    o_custkey AS customer_id,
+    o_orderdate AS order_date,
+    o_totalprice AS order_total,
+    o_orderstatus AS order_status,
+    o_orderpriority AS order_priority
+FROM
+    snowflake_sample_data.tpch_sf1.orders
